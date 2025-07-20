39e61328eb619e58bf779fba289678b1ca297c52
Henry Ochieng <111287785+Hencheng2@users.noreply.github.com>
2025-07-21 00:10:50 +0300

    Update movies.js

--- data/movies.js
+++ data/movies.js
@@ -1,48 +1,128 @@
 // data/movies.js
+
+// IMPORTANT: Ensure the 'themes' array is defined here for script.js to use
+const themes = ["Action", "Comedy", "Drama", "Sci-Fi", "Horror", "Thriller", "Adventure", "Fantasy", "Animation", "Documentary", "Mystery", "Romance"];
+
 const movies = [
     {
-       id: 'dave_and_the_kill_nod',
-       name: 'Dave and the Kill Nod',
-       theme: 'Comedy',
-       length: '30 sec',
-       type: 'Meme', // <-- ENSURE THIS IS 'Movie', 'Series', 'Anime', or 'Meme' as appropriate
-       year: 2023,
-       image: 'movie_images/dave_kill_nod.jpg',
-       video: 'k2FqgVQHYZlAZiDsv8a'
+        id: 'fast_x',
+        name: 'Fast X',
+        theme: 'Action',
+        length: '2h 21m',
+        type: 'Movie',
+        year: 2023,
+        image: 'movie_images/fastx.jpg',
+        video: 'x8my5j4'
     },
     {
-        id: 'action_movie_title',
-        name: 'The Great Escape',
+        id: 'spider_man_across_the_spider_verse',
+        name: 'Spider-Man: Across the Spider-Verse',
+        theme: 'Animation',
+        length: '2h 20m',
+        type: 'Movie',
+        year: 2023,
+        image: 'movie_images/spider-man.jpg',
+        video: 'x8kqw8p'
+    },
+    {
+        id: 'black_panther_wakanda_forever',
+        name: 'Black Panther: Wakanda Forever',
+        theme: 'Action',
+        length: '2h 41m',
+        type: 'Movie',
+        year: 2022,
+        image: 'movie_images/blackpanther.jpg',
+        video: 'x8g82j7'
+    },
+    {
+        id: 'dave_and_the_kill_nod',
+        name: 'Dave and the Kill Nod',
+        theme: 'Comedy',
+        length: '30 sec',
+        type: 'Meme',
+        year: 2023,
+        image: 'movie_images/dave_kill_nod.jpg',
+        video: 'k2FqgVQHYZlAZiDsv8a'
+    },
+    {
+        id: 'the_flash',
+        name: 'The Flash',
+        theme: 'Action',
+        length: '2h 24m',
+        type: 'Movie',
+        year: 2023,
+        image: 'movie_images/theflash.jpg',
+        video: 'x8mwq6s'
+    },
+    {
+        id: 'john_wick_chapter_4',
+        name: 'John Wick: Chapter 4',
+        theme: 'Action',
+        length: '2h 49m',
+        type: 'Movie',
+        year: 2023,
+        image: 'movie_images/johnwick.jpg',
+        video: 'x8k8o4r'
+    },
+    {
+        id: 'blue_beetle',
+        name: 'Blue Beetle',
+        theme: 'Action',
+        length: '2h 7m',
+        type: 'Movie',
+        year: 2023,
+        image: 'movie_images/bluebeetle.jpg',
+        video: 'x8o990r'
+    },
+    {
+        id: 'no_hard_feelings',
+        name: 'No Hard Feelings',
+        theme: 'Comedy',
+        length: '1h 43m',
+        type: 'Movie',
+        year: 2023,
+        image: 'movie_images/nohardfeelings.jpg',
+        video: 'x8m21lq'
+    },
+    {
+        id: 'extraction_2',
+        name: 'Extraction 2',
+        theme: 'Action',
+        length: '2h 2m',
+        type: 'Movie',
+        year: 2023,
+        image: 'movie_images/extraction2.jpg',
+        video: 'x8lu81m'
+    },
+    {
+        id: 'the_marvels',
+        name: 'The Marvels',
         theme: 'Action',
         length: '1h 45m',
-        type: 'Movie', // <-- Example of a 'Movie'
-        year: 2024,
-        image: 'movie_images/great_escape.jpg',
-        video: 'xACTIONID'
+        type: 'Movie',
+        year: 2023,
+        image: 'movie_images/themarvels.jpg',
+        video: 'x8pm48p'
     },
     {
-        id: 'anime_series_ep1',
-        name: 'Dragon Ball Z - Ep 1',
+        id: 'transformers_rise_of_the_beasts',
+        name: 'Transformers: Rise of the Beasts',
         theme: 'Action',
-        length: '24m',
-        type: 'Anime', // <-- Example of an 'Anime'
-        year: 1990,
-        image: 'movie_images/dbz_ep1.jpg',
-        video: 'xANIMEID'
+        length: '2h 7m',
+        type: 'Movie',
+        year: 2023,
+        image: 'movie_images/transformers.jpg',
+        video: 'x8mlj3b'
     },
     {
-        id: 'sitcom_episode_s1e1',
-        name: 'Friends - The Pilot',
-        theme: 'Comedy',
-        length: '22m',
-        type: 'Series', // <-- Example of a 'Series'
-        year: 1994,
-        image: 'movie_images/friends_s1e1.jpg',
-        video: 'xSERIESID'
+        id: 'godzilla_minus_one',
+        name: 'Godzilla Minus One',
+        theme: 'Action',
+        length: '2h 5m',
+        type: 'Movie',
+        year: 2023,
+        image: 'movie_images/godzillaminusone.jpg',
+        video: 'k5sM673FpS8x3kZ3LqG'
     },
-    // ... add more movies with correct 'type' values
-];
-
-const themes = [
-    // ... your existing themes
+    // Add more movies here following the same structure
 ];
